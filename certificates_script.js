e9338a6f0f1a9c5f9423312c6f1dbb8aa6959d0a
Mohamed <mohamed.kholy2011@gmail.com>
2025-03-29 10:56:39 +0200

    Update

--- certificates_script.js
+++ certificates_script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
             "hours": 8,
             "institute": "LinkedIn",
             "date": "March 2025",
-            "image": "./assets/Certificates/LinkedIn/Flutter Essential Training.JPG",
+            "image": "./assets/Certificates/LinkedIn/Flutter Essential Training.PNG",
             "pdf": "./assets/Certificates/LinkedIn/Flutter Essential Training.pdf",
             "verification": "https://www.linkedin.com/learning/certificates/66bc09271dac391da101552f40cc495012fd903cbc86bf9defe985546fd4e354?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3B2%2FcYT4GCR1GJooGZoRf24w%3D%3D"
         },
